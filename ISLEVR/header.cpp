2e98d07bde7f4d82c63eefae09f5ddc749bdd9fe
Lyelye150 <elijahdesa@gmail.com>
2025-08-24 01:17:16 -0400

    update

--- ISLEVR/header.cpp
+++ ISLEVR/header.cpp
@@ -1,17 +1,21 @@
 #include "header.h"
+#include <SDL3/SDL.h>
 #include <iostream>
 
-#ifdef MINIWIN
-#include "miniwin.h"
-#endif
-
-bool isVRHeadsetConnected() { return false; }
+bool isVRHeadsetConnected() {
+    return false;
+}
 
 void showVRNotDetectedMessage() {
-#ifdef MINIWIN
-    mwInit();
-    mwMessageBox("VR Headset Not Detected",
-                 "Please connect a VR headset before running this application.");
+#ifdef VRDEC
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+        std::cerr << "Failed to initialize SDL3: " << SDL_GetError() << "\n";
+    }
+
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION,
+                             "VR Headset Not Detected",
+                             "Please connect a VR headset before running this application.",
+                             nullptr);
 #else
     std::cerr << "ERROR: VR Headset not detected. Please connect a VR headset.\n";
 #endif
