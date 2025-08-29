41cae8bc4da720a93deabb4de78c059252ee7dff
Lyelye150 <elijahdesa@gmail.com>
2025-08-28 23:14:40 -0400

    update

--- ISLEVR/VRRen.cpp
+++ ISLEVR/VRRen.cpp
@@ -1,7 +1,7 @@
 #define XR_USE_PLATFORM_WIN32
 #define XR_USE_GRAPHICS_API_OPENGL
+#define WIN32_LEAN_AND_MEAN
 
-#include <windows.h>
 #include <Unknwn.h>
 #include <openxr/openxr.h>
 #include <openxr/openxr_platform.h>
