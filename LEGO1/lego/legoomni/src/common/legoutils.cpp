0018852830a8d3f91cdfc414d7467fc0d2ebb870
Lyelye150 <elijahdesa@gmail.com>
2025-09-13 15:36:28 -0400

    update

--- LEGO1/lego/legoomni/src/common/legoutils.cpp
+++ LEGO1/lego/legoomni/src/common/legoutils.cpp
@@ -32,7 +32,11 @@
 #include "scripts.h"
 
 #include <SDL3/SDL_events.h>
+#ifdef __WIIU__
+#include <SDL3/SDL.h>
+#else
 #include <SDL3/SDL_process.h>
+#endif
 #include <SDL3/SDL_stdinc.h>
 #include <stdio.h>
 #include <string.h>
