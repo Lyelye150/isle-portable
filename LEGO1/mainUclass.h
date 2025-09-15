cae6f341786eacd4f7dce46a8ec217cd8d110259
Lyelye150 <elijahdesa@gmail.com>
2025-09-15 19:53:25 -0400

    clang-format update

--- LEGO1/mainUclass.h
+++ LEGO1/mainUclass.h
@@ -3,24 +3,23 @@
 
 class Lego1App {
 public:
-    Lego1App() {
+	Lego1App() {}
+
+	~Lego1App() {}
+
+	void Run()
+	{
+		SDL_Log("Lego1App::Run called");
+
+		bool running = true;
+		SDL_Event e;
+
+		while (running) {
+			while (SDL_PollEvent(&e)) {
+				if (e.type == SDL_QUIT) {
+					running = false;
+				}
+			}
+		}
 	}
-
-    ~Lego1App() {
-	}
-
-    void Run() {
-        SDL_Log("Lego1App::Run called");
-
-        bool running = true;
-        SDL_Event e;
-
-        while (running) {
-            while (SDL_PollEvent(&e)) {
-                if (e.type == SDL_QUIT) {
-                    running = false;
-                }
-            }
-        }
-    }
 };
