a9898bb3cf03d4230e0517d4db177e239e5bc4de
Lyelye150 <elijahdesa@gmail.com>
2025-09-15 22:48:16 -0400

    update made gx2 renderer so...

--- LEGO1/mxdirectx/legodxinfo.h
+++ LEGO1/mxdirectx/legodxinfo.h
@@ -3,32 +3,10 @@
 
 #include "mxdirectxinfo.h"
 
-#ifdef __WIIU__
-class MxDeviceEnumerate {
-public:
-	MxDeviceEnumerate() {}
-	virtual ~MxDeviceEnumerate() {}
-	virtual int DoEnumerate(void* hwnd) { return 0; }
-};
-
-class LegoDeviceEnumerate : public MxDeviceEnumerate {
-public:
-	int ParseDeviceName(const char* p_deviceId) { return -1; }
-	int ProcessDeviceBytes(int p_deviceNum, GUID& p_guid) { return 0; }
-	int GetDevice(int p_deviceNum, MxDriver*& p_driver, Direct3DDeviceInfo*& p_device) { return 0; }
-	int FormatDeviceName(char* p_buffer, const MxDriver* p_ddInfo, const Direct3DDeviceInfo* p_d3dInfo) const
-	{
-		return 0;
-	}
-	int BETA_1011cc65(int p_idx, char* p_buffer) { return 0; }
-	int GetBestDevice() { return 0; }
-	int FUN_1009d210() { return 0; }
-	unsigned char FUN_1009d3d0(Direct3DDeviceInfo& p_device) { return 0; }
-
-	LegoDeviceEnumerate() {}
-	~LegoDeviceEnumerate() {}
-};
-#else
+// VTABLE: CONFIG 0x4060e4
+// VTABLE: LEGO1 0x100d9cc8
+// VTABLE: BETA10 0x101befb4
+// SIZE 0x14
 class LegoDeviceEnumerate : public MxDeviceEnumerate {
 public:
 	int ParseDeviceName(const char* p_deviceId);
@@ -40,6 +18,5 @@ public:
 	int FUN_1009d210();
 	unsigned char FUN_1009d3d0(Direct3DDeviceInfo& p_device);
 };
-#endif
 
 #endif // LEGODXINFO_H
