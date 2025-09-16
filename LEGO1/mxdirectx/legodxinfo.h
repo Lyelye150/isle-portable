140019bb7933d2c97e7ceb45007431bf82995a82
Lyelye150 <elijahdesa@gmail.com>
2025-09-15 20:14:32 -0400

    update

--- LEGO1/mxdirectx/legodxinfo.h
+++ LEGO1/mxdirectx/legodxinfo.h
@@ -3,10 +3,22 @@
 
 #include "mxdirectxinfo.h"
 
-// VTABLE: CONFIG 0x4060e4
-// VTABLE: LEGO1 0x100d9cc8
-// VTABLE: BETA10 0x101befb4
-// SIZE 0x14
+#ifdef __WIIU__
+class LegoDeviceEnumerate : public MxDeviceEnumerate {
+public:
+    int ParseDeviceName(const char* p_deviceId) { return -1; }
+    int ProcessDeviceBytes(int p_deviceNum, GUID& p_guid) { return 0; }
+    int GetDevice(int p_deviceNum, MxDriver*& p_driver, Direct3DDeviceInfo*& p_device) { return 0; }
+    int FormatDeviceName(char* p_buffer, const MxDriver* p_ddInfo, const Direct3DDeviceInfo* p_d3dInfo) const { return 0; }
+    int BETA_1011cc65(int p_idx, char* p_buffer) { return 0; }
+    int GetBestDevice() { return 0; }
+    int FUN_1009d210() { return 0; }
+    unsigned char FUN_1009d3d0(Direct3DDeviceInfo& p_device) { return 0; }
+
+    LegoDeviceEnumerate() {}
+    ~LegoDeviceEnumerate() {}
+};
+#else
 class LegoDeviceEnumerate : public MxDeviceEnumerate {
 public:
 	int ParseDeviceName(const char* p_deviceId);
@@ -17,13 +29,7 @@ public:
 	int GetBestDevice();
 	int FUN_1009d210();
 	unsigned char FUN_1009d3d0(Direct3DDeviceInfo& p_device);
-
-	// SYNTHETIC: BETA10 0x100d8d10
-	// LegoDeviceEnumerate::LegoDeviceEnumerate
-
-	// SYNTHETIC: LEGO1 0x1007b590
-	// SYNTHETIC: BETA10 0x100d8da0
-	// LegoDeviceEnumerate::~LegoDeviceEnumerate
 };
+#endif
 
 #endif // LEGODXINFO_H
