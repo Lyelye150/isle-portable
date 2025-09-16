adbce304bd1cb5490274633b6d30183eb30de9b3
Lyelye150 <elijahdesa@gmail.com>
2025-09-15 20:17:37 -0400

    clang-format update

--- LEGO1/mxdirectx/legodxinfo.h
+++ LEGO1/mxdirectx/legodxinfo.h
@@ -6,17 +6,20 @@
 #ifdef __WIIU__
 class LegoDeviceEnumerate : public MxDeviceEnumerate {
 public:
-    int ParseDeviceName(const char* p_deviceId) { return -1; }
-    int ProcessDeviceBytes(int p_deviceNum, GUID& p_guid) { return 0; }
-    int GetDevice(int p_deviceNum, MxDriver*& p_driver, Direct3DDeviceInfo*& p_device) { return 0; }
-    int FormatDeviceName(char* p_buffer, const MxDriver* p_ddInfo, const Direct3DDeviceInfo* p_d3dInfo) const { return 0; }
-    int BETA_1011cc65(int p_idx, char* p_buffer) { return 0; }
-    int GetBestDevice() { return 0; }
-    int FUN_1009d210() { return 0; }
-    unsigned char FUN_1009d3d0(Direct3DDeviceInfo& p_device) { return 0; }
+	int ParseDeviceName(const char* p_deviceId) { return -1; }
+	int ProcessDeviceBytes(int p_deviceNum, GUID& p_guid) { return 0; }
+	int GetDevice(int p_deviceNum, MxDriver*& p_driver, Direct3DDeviceInfo*& p_device) { return 0; }
+	int FormatDeviceName(char* p_buffer, const MxDriver* p_ddInfo, const Direct3DDeviceInfo* p_d3dInfo) const
+	{
+		return 0;
+	}
+	int BETA_1011cc65(int p_idx, char* p_buffer) { return 0; }
+	int GetBestDevice() { return 0; }
+	int FUN_1009d210() { return 0; }
+	unsigned char FUN_1009d3d0(Direct3DDeviceInfo& p_device) { return 0; }
 
-    LegoDeviceEnumerate() {}
-    ~LegoDeviceEnumerate() {}
+	LegoDeviceEnumerate() {}
+	~LegoDeviceEnumerate() {}
 };
 #else
 class LegoDeviceEnumerate : public MxDeviceEnumerate {
