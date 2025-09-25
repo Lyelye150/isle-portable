e6357fffa8e656e1772b78bf23f3424585e3dc95
Lyelye150 <elijahdesa@gmail.com>
2025-09-24 21:33:16 -0400

    update

--- miniwin/src/d3drm/backends/gx2/renderer.cpp
+++ miniwin/src/d3drm/backends/gx2/renderer.cpp
@@ -1,24 +1,25 @@
+#include <cstdio>
 #include <coreinit/systeminfo.h>
 #include <coreinit/thread.h>
 #include <coreinit/time.h>
 #include <gfd.h>
 #include <gx2/draw.h>
 #include <gx2/mem.h>
+#include <whb/gfx.h>
 #include <whb/log_udp.h>
 #include <whb/proc.h>
 #include <whb/sdcard.h>
 
 static const float sPositionData[] = {1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f};
-
 static const float sColourData[] = {1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f};
 
 int main(int argc, char** argv)
 {
-	GX2RBuffer positionBuffer = {0};
-	GX2RBuffer colourBuffer = {0};
-	WHBGfxShaderGroup group = {0};
-	void* buffer = NULL;
-	char* gshFileData = NULL;
+	GX2RBuffer positionBuffer = {};
+	GX2RBuffer colourBuffer = {};
+	WHBGfxShaderGroup group = {};
+	void* buffer = nullptr;
+	char* gshFileData = nullptr;
 	char path[256];
 	int result = 0;
 
@@ -32,7 +33,7 @@ int main(int argc, char** argv)
 	}
 
 	sprintf(path, "%s/wiiu/isle-U/content/renderer.gsh", WHBGetSdCardMountPath());
-	gshFileData = WHBReadWholeFile(path, NULL);
+	gshFileData = WHBReadWholeFile(path, nullptr);
 	if (!gshFileData) {
 		result = -1;
 		goto exit;
@@ -48,32 +49,32 @@ int main(int argc, char** argv)
 	WHBGfxInitFetchShader(&group);
 
 	WHBFreeWholeFile(gshFileData);
-	gshFileData = NULL;
+	gshFileData = nullptr;
 
 	positionBuffer.flags = GX2R_RESOURCE_BIND_VERTEX_BUFFER | GX2R_RESOURCE_USAGE_CPU_READ |
 						   GX2R_RESOURCE_USAGE_CPU_WRITE | GX2R_RESOURCE_USAGE_GPU_READ;
-	positionBuffer.elemSize = 4 * 4;
+	positionBuffer.elemSize = sizeof(float) * 4;
 	positionBuffer.elemCount = 3;
 	GX2RCreateBuffer(&positionBuffer);
-	buffer = GX2RLockBufferEx(&positionBuffer, 0);
+	buffer = GX2RLockBufferEx(&positionBuffer, GX2R_RESOURCE_USAGE_CPU_WRITE);
 	memcpy(buffer, sPositionData, positionBuffer.elemSize * positionBuffer.elemCount);
-	GX2RUnlockBufferEx(&positionBuffer, 0);
+	GX2RUnlockBufferEx(&positionBuffer, GX2R_RESOURCE_USAGE_CPU_WRITE);
 
 	colourBuffer.flags = GX2R_RESOURCE_BIND_VERTEX_BUFFER | GX2R_RESOURCE_USAGE_CPU_READ |
 						 GX2R_RESOURCE_USAGE_CPU_WRITE | GX2R_RESOURCE_USAGE_GPU_READ;
-	colourBuffer.elemSize = 4 * 4;
+	colourBuffer.elemSize = sizeof(float) * 4;
 	colourBuffer.elemCount = 3;
 	GX2RCreateBuffer(&colourBuffer);
-	buffer = GX2RLockBufferEx(&colourBuffer, 0);
+	buffer = GX2RLockBufferEx(&colourBuffer, GX2R_RESOURCE_USAGE_CPU_WRITE);
 	memcpy(buffer, sColourData, colourBuffer.elemSize * colourBuffer.elemCount);
-	GX2RUnlockBufferEx(&colourBuffer, 0);
+	GX2RUnlockBufferEx(&colourBuffer, GX2R_RESOURCE_USAGE_CPU_WRITE);
 
 	while (WHBProcIsRunning()) {
-		float* colours = (float*) GX2RLockBufferEx(&colourBuffer, 0);
+		float* colours = (float*) GX2RLockBufferEx(&colourBuffer, GX2R_RESOURCE_USAGE_CPU_WRITE);
 		for (int i = 0; i < 12; i++) {
 			colours[i] = colours[i] >= 1.0f ? 0.0f : (colours[i] + 0.01f);
 		}
-		GX2RUnlockBufferEx(&colourBuffer, 0);
+		GX2RUnlockBufferEx(&colourBuffer, GX2R_RESOURCE_USAGE_CPU_WRITE);
 
 		WHBGfxBeginRender();
 
@@ -101,8 +102,8 @@ int main(int argc, char** argv)
 	}
 
 exit:
-	GX2RDestroyBufferEx(&positionBuffer, 0);
-	GX2RDestroyBufferEx(&colourBuffer, 0);
+	GX2RDestroyBufferEx(&positionBuffer, GX2R_RESOURCE_USAGE_CPU_WRITE);
+	GX2RDestroyBufferEx(&colourBuffer, GX2R_RESOURCE_USAGE_CPU_WRITE);
 	WHBUnmountSdCard();
 	WHBGfxShutdown();
 	WHBProcShutdown();
